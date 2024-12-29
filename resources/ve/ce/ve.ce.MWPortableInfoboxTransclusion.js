d16bbee7da0e8c10be22b3d065736501c486f358
OriginalAuthority <luke@telepedia.net>
2024-12-29 20:08:57 +0000

    Show PortableInfoboxes in VE

---
+++ resources/ve/ce/ve.ce.MWPortableInfoboxTransclusion.js
@@ -0,0 +1,51 @@
+ve.ce.MWPortableInfoboxTransclusion = function VeCeMWPortableInfoboxTransclusion() {
+    ve.ce.MWPortableInfoboxTransclusion.super.apply( this, arguments );
+}
+
+OO.inheritClass( ve.ce.MWPortableInfoboxTransclusion, ve.ce.MWTransclusionBlockNode );
+
+ve.ce.MWPortableInfoboxTransclusion.static.primaryCommandName = 'mwVEPI';
+
+ve.ce.MWPortableInfoboxTransclusion.prototype.onSetup = function () {
+    const modelNode = this.getModel();
+
+    const mwData = modelNode.getAttribute('mw');
+
+    if (mwData && mwData.parts) {
+        // Extract the wikitext from the model -- must be a better way to do this, surely?
+        const params = [];
+        mwData.parts.forEach(part => {
+            if (part.template) {
+                Object.entries(part.template.params).forEach(([key, value]) => {
+                    params.push(`|${key}=${value.wt}`);
+                });
+            }
+        });
+
+        const wikitext = `{{${mwData.parts[0].template.target.wt}${params.join('')}}}`;
+
+        /**
+         * Send this wikitext to the MW API and pray we get HTML back
+         */
+        new mw.Api().post({
+            action: 'parse',
+            text: wikitext,
+            contentmodel: 'wikitext',
+            format: 'json'
+        }).then((response) => {
+            if (response.parse && response.parse.text) {
+                // replace the stub with the HTML, somehow its floated to the left, but I'm not sure why?!
+                const html = response.parse.text['*'];
+                this.$element.empty().append(html);
+            }
+        }).catch((error) => {
+            console.error('Failed to parse infobox:', error);
+        });
+    }
+
+    // Parent method
+    ve.ce.MWPortableInfoboxTransclusion.super.prototype.onSetup.call( this );
+};
+
+
+ve.ce.nodeFactory.register( ve.ce.MWPortableInfoboxTransclusion );
